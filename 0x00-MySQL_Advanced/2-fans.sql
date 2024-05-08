6ad0dd96a9a724ccdf6fdc475e2e94881e651f2e
Navygeta <tinashenavonika@gmail.com>
2024-05-09 00:16:29 +0200

    fans

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -4,7 +4,6 @@ SELECT origin, COUNT(*) AS nb_fans
 FROM metal_bands
 GROUP BY origin;
 
--- Rank the country origins of bands based on the number of fans
 SELECT origin, nb_fans
 FROM temp_band_fans_counts
 ORDER BY nb_fans DESC;
