43f7157ed93849b807a9ca94a866ab016157afcb
Navygeta <tinashenavonika@gmail.com>
2024-05-15 04:06:27 +0200

    log stats

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+"""Log Stats
+
+This script retrieves statistics from an Nginx log collection stored in a
+MongoDB database.
+
+Functions:
+    log_stats: Connects to the MongoDB database, retrieves the Nginx
+    log collection,and prints statistics regarding the log entries.
+"""
+
+from pymongo import MongoClient
+
+
+def log_stats():
+    """Retrieve statistics from the Nginx log collection.
+
+    This function connects to the MongoDB database where the Nginx logs are
+    stored,retrieves the log collection, and prints statistics regarding the
+    log entries. The statistics include the total number of log entries, the
+    count for each HTTP method, and the count of requests with the method
+    'GET' and path '/status'.
+
+    Args:
+        None
+
+    Returns:
+        None
+    """
+    # Connect to MongoDB and retrieve the log collection
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    logs_collection = client.logs.nginx
+
+    # Count total log entries
+    total = logs_collection.count_documents({})
+
+    # Count log entries for each HTTP method
+    get = logs_collection.count_documents({"method": "GET"})
+    post = logs_collection.count_documents({"method": "POST"})
+    put = logs_collection.count_documents({"method": "PUT"})
+    patch = logs_collection.count_documents({"method": "PATCH"})
+    delete = logs_collection.count_documents({"method": "DELETE"})
+
+    # Count log entries with method 'GET' and path '/status'
+    path = logs_collection.count_documents({
+        "method": "GET",
+        "path": "/status"
+    })
+
+    # Print statistics
+    print(f"{total} logs")
+    print("Methods:")
+    print(f"\tmethod GET: {get}")
+    print(f"\tmethod POST: {post}")
+    print(f"\tmethod PUT: {put}")
+    print(f"\tmethod PATCH: {patch}")
+    print(f"\tmethod DELETE: {delete}")
+    print(f"{path} status check")
+
+
+if __name__ == "__main__":
+    log_stats()
