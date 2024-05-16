a38c70ea6d01af5b41fea0a9f441f202df2add68
Navygeta <tinashenavonika@gmail.com>
2024-05-16 04:00:51 +0200

    web 3

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,52 +1,38 @@
 #!/usr/bin/env python3
-"""
-Caching request module
-"""
+'''A module with tools for request caching and tracking.
+'''
 import redis
 import requests
 from functools import wraps
 from typing import Callable
 
 
-def track_get_page(fn: Callable) -> Callable:
-    """Decorator to track and cache HTTP GET requests.
-
-    Args:
-        fn (Callable): The function to be decorated.
-
-    Returns:
-        Callable: The decorated function.
-    """
-    @wraps(fn)
-    def wrapper(url: str) -> str:
-        """Wrapper function to cache and track HTTP GET requests.
-
-        Args:
-            url (str): The URL to request data from.
-
-        Returns:
-            str: The response content.
-        """
-        client = redis.Redis()
-        client.incr(f'count:{url}')
-        cached_page = client.get(f'{url}')
-        if cached_page:
-            return cached_page.decode('utf-8')
-        response = fn(url)
-        client.set(f'{url}', response, 10)
-        return response
-    return wrapper
+redis_store = redis.Redis()
+'''The module-level Redis instance.
+'''
 
 
-@track_get_page
+def data_cacher(method: Callable) -> Callable:
+    '''Caches the output of fetched data.
+    '''
+    @wraps(method)
+    def invoker(url) -> str:
+        '''The wrapper function for caching the output.
+        '''
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
+        return result
+    return invoker
+
+
+@data_cacher
 def get_page(url: str) -> str:
-    """Makes an HTTP GET request to the specified URL.
-
-    Args:
-        url (str): The URL to request data from.
-
-    Returns:
-        str: The response content.
-    """
-    response = requests.get(url)
-    return response.text
+    '''Returns the content of a URL after caching the request's response,
+    and tracking the request.
+    '''
+    return requests.get(url).text
